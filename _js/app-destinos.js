f1966b6ccea86c55397050c45470fd953026d952
Andrea Tejero <iandreatejero@gmail.com>
2025-02-28 12:01:47 +0100

    Arreglos y optimizaciones

--- _js/app-destinos.js
+++ _js/app-destinos.js
@@ -1,20 +1,5 @@
 
-/*
-
-En este js están:
-1.Dropdown Usuario y Buscar (header principal)
-2.Menú de navegación Manual
-3.Modales: Iniciar sesión y crear cuenta
-
-*/
-
-
-/* -------------------------------------- */
-
-
 //HEADER DROPDOWNS
-
-/* En el header general de la página, están los dropdown de buscar y el otro del usuario (iniciar sesión y crear cuenta */
 
 //Dropdown Usuario (Iniciar sesión y crear cuenta)
 let MenuUser = document.getElementById("MenuUser"); //Creamos una variable para el botón
@@ -37,21 +22,20 @@ function toggleMenuSearch(){ //Aplicamos la función toggle que al hacer click, 
 
 //MENÚ DE NAVEGACIÓN LATERAL BOTONES (NAVEGACIÓN MANUAL)
 
-/* Estos botones manuales permiten al usuario navegar por la página una vez que se haya hecho click en cada uno de ellos */
 
 //Creamos variables para todos los contenedores y todos los botones
 var contenedores = document.querySelectorAll(".contenedor");
 var btns = document.querySelectorAll(".btn");
 let currentContenedores = 1;
 
-var manualNav = function(manual){ //Añadimos una función manual para cada contenedor de la página
+var manualNav = function(manual) { 
     contenedores.forEach((contenedor) => {
-        contenedor.classList.remove("active"); //Removemos la clase "active" para cada contenedor
-    
-    btns.forEach((btn) => { //Removemos la clase "active" para cada botón
+        contenedor.classList.remove("active"); 
+    });
+
+    btns.forEach((btn) => { 
         btn.classList.remove("active");
     });
-});
 
     contenedores[manual].classList.add("active"); //Añadimos la clase "active" a los contenedores junto con la función manual
     btns[manual].classList.add("active"); //Añadimos la clase "active" a los botones junto con la función manual
@@ -86,7 +70,9 @@ document.querySelector(".login #close-login").addEventListener("click", function
     document.querySelector(".login").classList.remove("show");
 });
 
+
 /* -------------------------------------- */
+
 
 //MODAL REGISTRO
 
